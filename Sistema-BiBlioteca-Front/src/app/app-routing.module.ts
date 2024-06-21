740ce9d82d343a2e1eb5b28d6288d935ba9681e5
SaulReyes-ux <79472319+SaulReyes-ux@users.noreply.github.com>
2024-06-21 12:58:19 -0600

    Vista de lista de libros

--- Sistema-BiBlioteca-Front/src/app/app-routing.module.ts
+++ Sistema-BiBlioteca-Front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaLibrosComponent } from './componentes/administrador/lista-libros/lista-libros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'lista_libros',
+    component: ListaLibrosComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Sistema-BiBlioteca-Front/src/app/componentes/administrador/lista-libros/lista-libros.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaLibrosComponent } from './lista-libros.component';
+
+describe('ListaLibrosComponent', () => {
+  let component: ListaLibrosComponent;
+  let fixture: ComponentFixture<ListaLibrosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ListaLibrosComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListaLibrosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
